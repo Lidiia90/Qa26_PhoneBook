de57214bf87f7e1ff7fcd60a49e5ad527952a293
Lidiia90 <liduska200412@gmail.com>
2024-07-03 16:27:40 +0300

    logout

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,9 +1,18 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
+
+    @BeforeMethod
+    public void preCondition(){
+        //if SIGN OUT present ----> logout
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
 
     @Test
     public void loginSuccess() {
@@ -18,4 +27,13 @@ public class LoginTests extends TestBase {
 
         Assert.assertTrue(app.getHelperUser().isLogged());
     }
+
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("kate24@gmail.com", "kaT45#kit");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
 }
+
